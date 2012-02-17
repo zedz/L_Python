46a9e8a516ad51ea15794f21dec9608bfdc2c2b7
Jamie <j.zh.cheer@gmail.com>
2012-02-16 22:27:08 -0500

    Revert "Revert "ex 5 - ex 14""
    This reverts commit 182e1384f32ff227eb1ffa0eb66b94136613f8fe.

---
+++ ex_py/ex13.py
@@ -0,0 +1,23 @@
+###invalid syntax###
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+python ex13.py first 2nd 3rd
+
+#python ex13.py aa bb cc
+
+
+
+#os, bro, loc, time =argv
+
+#print "Your OS is: ", os
+#print "Your browser is: ", bro
+#print "Your location is: ", loc
+#print "Your local time is: ", time
